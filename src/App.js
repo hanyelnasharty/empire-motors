eefdf8f9bfe4180149408c98a86dac09851441b6
Devorius <Devoriusharris23@gmail.com>
2023-04-03 20:21:16 -0500

    Frontend

--- src/App.js
+++ src/App.js
@@ -49,23 +49,25 @@ const App = () => {
   }, [])
 
   return (
-  <>
-    <h1>Cars Showroom</h1>
-    <Add handleCreate={handleCreate}/>
-    <div className='container'>
-    {cars.map((cars) => {
-      return (
-        <>
-          <div className='home'>
-            <Cars cars={cars} />
-            <Edit cars={cars} handleEdit={handleEdit} />
-            <button onClick={ () => {handleDelete(cars) } }>Delete</button>
-          </div>
-        </>
-      )
-    })}
-    </div>
-  </>
+    <>
+      <img class="bg" src="./image/ShowroomBG.jpg" alt= ""/>
+      
+      <h1>Cars Showroom</h1>
+      <Add handleCreate={handleCreate} />
+      <div className='container'>
+        {cars.map((cars) => {
+          return (
+            <>
+              <div className='home'>
+                <Cars cars={cars} />
+                <Edit cars={cars} handleEdit={handleEdit} />
+                <button onClick={() => { handleDelete(cars) }}>Delete</button>
+              </div>
+            </>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
